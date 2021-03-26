f4f906ee85416776a43609db90e028e9fb9b6693
anpopov <anpopov@at-consulting.ru>
2021-03-26 11:24:54 +0300

    Added api project

---
+++ clay-api/src/main/java/space/ffisherr/clayapi/ClayApiApplication.java
@@ -0,0 +1,13 @@
+package space.ffisherr.clayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClayApiApplication.class, args);
+	}
+
+}
---
+++ clay-api/src/test/java/space/ffisherr/clayapi/ClayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package space.ffisherr.clayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
