1d21bd91ea5bb470cab92a320355e37a7a4b96e3
anpopov <anpopov@at-consulting.ru>
2021-03-26 10:54:26 +0300

    initialize project

---
+++ src/test/java/space/ffisherr/clay/ClayApplicationTests.java
@@ -0,0 +1,13 @@
+package space.ffisherr.clay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
